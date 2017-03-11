261b6d67165aedd5dd63f497fa8b9aa2e5cb8cdb
Azraelki <584917180@qq.com>
2017-03-11 14:58:10 +0800

    build orm module

---
+++ www/transwarp/orm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+__author__ = 'azrael'
+
+
+'''
+Database operation module. This module is independent with web module.
+'''
+
+import time, logging
+import db
